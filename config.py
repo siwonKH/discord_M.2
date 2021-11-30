b3bb5f0e772d2739e88d5ca3f2b64a0fb236e268
siwonKH <kanghyunkim313@gmail.com>
2021-12-01 08:59:19 +0900

    variables to capital

--- config.py
+++ config.py
@@ -1,34 +1,36 @@
 # ==========================================#
 # 메시지 보낼 시간 (시간)
-set_hour = 0
+SET_HOUR = 0
 
 # 메시지 보낼 시간 (분)
-set_min = 0
+SET_MIN = 0
 
 # 메시지 보내기전 준비시간(분)  # 1보다 커야함
-margin_minutes = 1
+MARGIN_MINUTES = 1
 
 # API 학교 종류
-sc_type = "high"
+SC_TYPE = "high"
 
 # API 학교 코드
-code = "R100000822"
+CODE = "R100000822"
+
+# 가사 분할 크기
+PARSE_LEN = 1000
 # ==========================================#
 
 
-print("Checking config file")
-if set_hour < 0 or set_hour > 23:
+if SET_HOUR < 0 or SET_HOUR > 23:
     raise Exception("Invalid Config: set_hour")
-if set_min < 0 or set_min > 59:
+if SET_MIN < 0 or SET_MIN > 59:
     raise Exception("Invalid Config: set_min")
-if margin_minutes < 1:
+if MARGIN_MINUTES < 1:
     raise Exception("Invalid Config: margin_minutes")
 
 # Set Ready Time
-ready_hour = set_hour
-ready_min = set_min - margin_minutes
-if ready_min < 0:
-    ready_min += 60
-    ready_hour -= 1
-    if ready_hour < 0:
-        ready_hour += 24
+READY_HOUR = SET_HOUR
+READY_MIN = SET_MIN - MARGIN_MINUTES
+if READY_MIN < 0:
+    READY_MIN += 60
+    READY_HOUR -= 1
+    if READY_HOUR < 0:
+        READY_HOUR += 24
